b89a4f00617c34f2dd86b8d00e91e87ec75c7a4d
Sora Arakawa <mail@arkw.net>
2022-06-12 23:16:43 +0900

    Use sprite to draw screen

--- M5StickClock.ino
+++ M5StickClock.ino
@@ -4,22 +4,32 @@
 #include <M5StickC.h>
 #include "img/logo.h"
 #include "img/layout.h"
+#include "img/big.h"
 
 int count = 0;
+TFT_eSprite sprite(&M5.Lcd);
 
 void setup() {
   M5.begin();
   M5.Lcd.setRotation(1);
-  M5.Lcd.print("M5StickClock");
+  M5.Lcd.setSwapBytes(true);
+  sprite.createSprite(M5.Lcd.width(), M5.Lcd.height());
+  sprite.setSwapBytes(true);
 }
 
 void loop() {
   M5.Lcd.startWrite();
+  sprite.fillScreen(BLACK);
   if (count < 20) {
-    M5.Lcd.pushImage(0, 0, logoWidth, logoHeight, logo);
+    sprite.pushImage(0, 0, logoWidth, logoHeight, logo);
   } else {
-    M5.Lcd.pushImage(0, 0, layoutWidth, layoutHeight, layout);
+    sprite.pushImage(0, 0, layoutWidth, layoutHeight, layout);
+    sprite.pushImage(4, 12, bigWidth, bigHeight, big);
+    sprite.pushImage(31, 12, bigWidth, bigHeight, big);
+    sprite.pushImage(74, 12, bigWidth, bigHeight, big);
+    sprite.pushImage(101, 12, bigWidth, bigHeight, big);
   }
+  sprite.pushSprite(0, 0);
   M5.Lcd.endWrite();
   count++;
   delay(1000 / 20);
